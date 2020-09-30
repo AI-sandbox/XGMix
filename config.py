00b9eec9013297812219395b60b6b3485e9191c1
weekend37 <helgi37@gmail.com>
2020-09-30 14:46:35 -0700

    update config for train1, train2

--- config.py
+++ config.py
@@ -3,15 +3,16 @@ verbose = True     # logging
 instance_name = "" # can be used for organizing simulation output and models
 
 # Simulatiation configuration
-run_simulation = True     # if you already simulated data, setting False will re-use it
-num_outs = [300, 100, 0]  # how many individuals in each sub-instance
-generations = [2, 4, 8, 16, 32, 64]  # what generations to generate during simulation
-rm_simulated_data = False # remove the simulated data after training the model
+run_simulation = True     # if you already simulated data, setting False will re-use the data
+founders_ratios = [0.6, 0.10, 0.15, 0.15]
+num_outs = [64, 10, 40, 40] # [320, 50, 80, 80]  # how many individuals in each set for each generation
+generations = [0, 2, 4, 6, 8, 12, 16, 24, 32, 48, 64]  # what generations to generate during simulation
+rm_simulated_data = False     # remove the simulated data after training the model
 
 # Model configuration
 model_name = "model" # the complete name will be <model_name>_chm<chm>.pkl
-window_size = 1000   # window size of the XGMix model
+window_size = 750    # window size of the XGMix model
 smooth_size = 75     # how many windows to aggregate over in second phase
 missing = 0.0        # fraction in [0,1) of how much to simulate missing data during training
-n_cores = 20         # how many units of cpu to use
-smooth_lite = 10000  # how many (random) snps the smoother uses
+retrain_base = True  # for retraining base models with [train1, train2] once the smoother has been trained with train2
+n_cores = 24         # how many units of cpu to use
